aed76c30b096291f4bee8e22faea78bb09e8487e
Christien Rioux <chris@veilid.org>
2024-08-01 14:52:10 -0500

    update dependencies

--- lib/theme/views/widget_helpers.dart
+++ lib/theme/views/widget_helpers.dart
@@ -8,7 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
-import 'package:meta/meta.dart';
 import 'package:quickalert/quickalert.dart';
 import 'package:sliver_expandable/sliver_expandable.dart';
 
